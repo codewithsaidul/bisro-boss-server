2f563ca121c7beee27f68fed7a6eca6c1823a60d
codewithsaidul <codewithsaidul@gmail.com>
2024-05-19 20:37:21 +0600

    Get the all menu data from db

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ app.use(express.json())
 // MongoDb Connection 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lggjuua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+
+
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -38,14 +40,26 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+
+
+    const menuCollection = client.db('bistroDB').collection('menu')
+    const reviewsCollection = client.db('bistroDB').collection('reviews')
+
+
+    // Get The All Menu Data From Database
+    app.get('/menu', async(req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result)
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
